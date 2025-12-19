ea04257e66b60b904fbe51fdaf1d87193b550db7
Sourav Nag <78099286+BlueOrchid37@users.noreply.github.com>
2025-12-19 09:12:17 +0530

    hand_cricket_game.py

--- HandCricket.py
+++ HandCricket.py
@@ -1,173 +1,93 @@
-#Hand Cricket Project
-
-#Toss Section
 import random
-toss=["Head","Tail"]
-comChoice=["Batting","Bowling"]
-playScore=[0,1,2,3,4,5,6]
-score=0
-comSumScore=0
-sumScore = 0
-bowlScore = 0
 
-playOrNot=int(input('''
-Play Hand Cricket?
-1 Yes
-2 No
-'''))
-print("You select: ", playOrNot)
-if playOrNot==1:
-    tossChoiceByUser=int(input('''
-    1 Head
-    2 Tail
-    '''))
-    comTossHT=random.choice(toss)
-    print(comTossHT)
-    if (tossChoiceByUser==1 and comTossHT=="Head") or (tossChoiceByUser==2 and comTossHT=="Tail"):
+RUNS = [0, 1, 2, 3, 4, 5, 6]
+
+
+def toss():
+    print("\nToss Time!")
+    user_choice = int(input("1 Head\n2 Tail\nChoose: "))
+    toss_result = random.choice(["Head", "Tail"])
+    print("Toss result:", toss_result)
+
+    if (user_choice == 1 and toss_result == "Head") or \
+       (user_choice == 2 and toss_result == "Tail"):
         print("You won the toss!")
-        
-        #Match Starts
-
-        tossChoice=int(input('''
-        1. Batting
-        2. Bowling
-        
-        '''))   
-        if tossChoice==1:
-            print("You chose Batting!")
-            score=int(input("Enter score: "))
-            comScore=random.choice(playScore)
-            
-            while score!=comScore:
-                sumScore=sumScore+score
-                score=int(input("Enter Run: "))
-                comScore=random.choice(playScore)
-            print("Out!")
-            print("Total Run: ", sumScore)
-            print("Target: ", sumScore + 1)
-            while bowlScore!=comScore and comSumScore<=sumScore:
-                comSumScore=comSumScore+bowlScore
-                bowlScore=int(input("Enter Bowl: "))
-                comScore=random.choice(playScore)
-                print(comScore) #1
-            if (bowlScore==comScore):
-                print("Computer Out!")
-            print("Total Run: ", comSumScore)         
-            if sumScore>comSumScore:
-                print("You won!")
-            elif comSumScore>sumScore:
-                print("Computer won!")
-            else:
-                print("Match draw!!")
-            
-            
-
-        elif tossChoice==2:
-            print("You chose to Bowling!")
-            bowlScore=int(input("Enter Bowl: "))
-            comScore=random.choice(playScore)
-            print("Computer Run:", comScore)
-            while bowlScore!=comScore:
-                comSumScore=comSumScore+bowlScore
-                bowlScore=int(input("Enter Bowl: "))
-                comScore=random.choice(playScore)
-                print("Computer Run: ", comScore)
-            print("Computer Out!")
-            print("Total Run: ", comSumScore)
-            print("Target: ", comSumScore + 1)
-            score=int(input("Enter Run: "))
-            comScore=random.choice(playScore)
-            while score!=comScore and sumScore<=comSumScore:
-                sumScore=sumScore+score
-                score=int(input("Enter Run: "))
-                comScore=random.choice(playScore)
-            if (score==comScore):
-                print("Out!")
-            print("Total Run: ", sumScore)
-            if sumScore>comSumScore:
-                print("You won!")
-            elif comSumScore>sumScore:
-                print("Computer won!")
-            else:
-                print("Match draw!!")
-            
-            
-            
-            
-        else:
-            print("Invalid Input! Match dismissed!!")
-        
+        return "user"
     else:
         print("Computer won the toss!")
-        comToss=random.choice(comChoice)
-        comScore=random.choice(playScore)
-        if comToss==comChoice[0]:
-            print("Computer chose Batting!")
-            bowlScore=int(input("Enter Bowl: "))
-            comScore=random.choice(playScore)
-            print("Computer Run: ", comScore)
-            while bowlScore!=comScore:
-                comSumScore=comSumScore+bowlScore
-                bowlScore=int(input("Enter Bowl: "))
-                comScore=random.choice(playScore)
-                print("Computer Run: ", comScore)
-                print(comScore)
-            print("Computer Out!")
-            print("Computer Total Run: ", comSumScore)
-            print("Target: ", comSumScore + 1)
-            score = int(input("Enter Run: "))
-            comScore = random.choice(playScore)
-            while score!=comScore and sumScore<=comSumScore:
-                sumScore=sumScore+score
-                score=int(input("Enter Run: "))
-                comScore=random.choice(playScore)
-            if (score==comScore):
-                print("Out!")
-            print("Your Total Run: ", sumScore)
-            if sumScore>comSumScore:
-                print("You won!")
-            elif comSumScore>sumScore:
-                print("Computer won!")
-            else:
-                print("Match draw!!")
-            
-            
-            
-            
-        elif comToss==comChoice[1]:
-            print("Computer chose Bowling!")
-            score=int(input("Enter Run: "))
-            comScore=random.choice(playScore)
-            while score!=comScore:
-                sumScore=sumScore+score
-                score=int(input("Enter Run: "))
-                comScore=random.choice(playScore)
+        return "computer"
+
+
+def batting():
+    score = 0
+    while True:
+        user_run = int(input("Enter run: "))
+        comp_run = random.choice(RUNS)
+        if user_run == comp_run:
             print("Out!")
-            print("Your Total Run: ", sumScore)
-            print("Target: ", sumScore + 1)
-            bowlScore = int(input("Enter Bowl: "))
-            comScore = random.choice(playScore)
-            print("Computer Run: ", comScore)
-            while bowlScore!=comScore and comSumScore<=sumScore:
-                comSumScore=comSumScore+bowlScore
-                bowlScore=int(input("Enter Bowl: "))
-                comScore=random.choice(playScore)
-                print("Computer Run: ", comScore)
-            if (bowlScore==comScore):
-                print("Computer Out!")
-            print("Computer Total Run: ", comSumScore)
-            if sumScore>comSumScore:
-                print("You won!")
-            elif comSumScore>sumScore:
-                print("Computer won!")
-            else:
-                print("Match draw!!")
-            
-            
-    
-else:
-    print("Exit")
+            break
+        score += user_run
+    print("Total Score:", score)
+    return score
 
 
+def bowling(target=None):
+    score = 0
+    while target is None or score < target:
+        user_ball = int(input("Enter bowl: "))
+        comp_run = random.choice(RUNS)
+        print("Computer run:", comp_run)
+        if user_ball == comp_run:
+            print("Computer Out!")
+            break
+        score += comp_run
+    print("Computer Total:", score)
+    return score
 
 
+def decide_winner(user_score, comp_score):
+    if user_score > comp_score:
+        print("\nYou won the match!")
+    elif comp_score > user_score:
+        print("\nComputer won the match!")
+    else:
+        print("\nMatch Draw!")
+
+
+def main():
+    print("Welcome to Hand Cricket!")
+    play = int(input("1 Play\n2 Exit\nChoose: "))
+    if play != 1:
+        print("Exit")
+        return
+
+    toss_winner = toss()
+
+    if toss_winner == "user":
+        choice = int(input("1 Batting\n2 Bowling\nChoose: "))
+        if choice == 1:
+            user_score = batting()
+            print("Target:", user_score + 1)
+            comp_score = bowling(target=user_score + 1)
+        else:
+            comp_score = bowling()
+            print("Target:", comp_score + 1)
+            user_score = batting()
+    else:
+        comp_choice = random.choice(["Batting", "Bowling"])
+        print("Computer chose:", comp_choice)
+
+        if comp_choice == "Batting":
+            comp_score = bowling()
+            print("Target:", comp_score + 1)
+            user_score = batting()
+        else:
+            user_score = batting()
+            print("Target:", user_score + 1)
+            comp_score = bowling(target=user_score + 1)
+
+    decide_winner(user_score, comp_score)
+
+
+if __name__ == "__main__":
+    main()
